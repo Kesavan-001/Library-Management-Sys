6f1edf1697b9e3325a8b140c6e88ad9a04fffcf1
[Kesavan-001] <[kesavanp0395@gmail.com]>
2025-10-17 17:17:35 +0530

    new

--- library_management/www/dashboard.py
+++ library_management/www/dashboard.py
@@ -1,33 +1,41 @@
 import frappe
 
-def get_context(context):
-    context.members = frappe.get_all(
+def get_context(getdata):
+    getdata.members = frappe.get_all(
         "Library Member",
         fields=["name", "name1", "email", "mobile_no", "membership_start_date", "membership_end_date"],
         order_by="creation desc"
     )
-    context.books = frappe.get_all(
+    getdata.books = frappe.get_all(
         "Books",
         fields=["name", "title", "author", "status", "publisher", "available_copies", "cover_image"],
         order_by="creation desc"
     )
 
 
-    context.fines = frappe.get_all(
+    getdata.fines = frappe.get_all(
         "Fine",
         fields=["name", "member", "book", "amount", "reason", "date_of_fine", "paid"],
         order_by="creation desc"
     )
 
-    context.transactions = frappe.get_all(
+    getdata.transactions = frappe.get_all(
         "Membership Transaction",
         fields=["name", "member", "start_date", "end_date", "paid"],
         order_by="creation desc"
     )
 
-    context.total_members = len(context.members)
-    context.total_books = len(context.books)
-    context.total_fines = len(context.fines)
-    context.total_transactions = len(context.transactions)
+    getdata.bookissue = frappe.get_all(
+        "Book Issue",
+        fields=["name","member","custom_name","book","issue_date"],
+        order_by="creation desc"
+    )
 
-    return context
+
+    getdata.total_members = len(getdata.members)
+    getdata.total_books = len(getdata.books)
+    getdata.total_fines = len(getdata.fines)
+    getdata.total_transactions = len(getdata.transactions)
+    getdata.book_issue = len(getdata.bookissue)
+
+    return getdata
